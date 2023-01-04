a42d9ff2f7a64de8a5e1ec0f3b906e092cc88bcc
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-01-04 18:27:23 -0300

    Version final01

--- src/App.js
+++ src/App.js
@@ -2,11 +2,8 @@ import './App.scss';
 import Editor from './components/editor';
 function App() {
   return (
-    <div className="Container">
-     <h1>Bene Editor</h1>
-      <Editor />
-    </div>
-  );
+        <Editor />
+      );
 }
 
 export default App;
