5db7a88fd08a1d13608b60d264c25e49c448aa3c
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-01-04 18:14:25 -0300

    Finish

--- src/components/editor.js
+++ src/components/editor.js
@@ -16,7 +16,9 @@ const Editor = ({placeholder}) => {
         config={config}
         tabIndex={1} // tabIndex of textarea
         onBlur={newContent => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
-        onChange={newContent => {}}
+        onChange={newContent => {
+            console.log(newContent);
+        }}
         />
         </div>
     )
